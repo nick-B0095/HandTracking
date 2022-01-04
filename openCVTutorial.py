3d19e129d6564515131f34ebaec0c3bbb5bf6a0f
jay <64019067+nick-B0095@users.noreply.github.com>
2022-01-05 01:47:45 +0900

    Add files via upload

---
+++ openCVTutorial.py
@@ -0,0 +1,38 @@
+import cv2
+import time
+import os
+import HTM as htm
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+folderPath = "FingerImages"
+myList = os.listdir(folderPath)
+print(myList)
+
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+
+pTime = 0
+
+# detector = htm.handDetector(detectionCon=0.75)
+
+while True:
+    success, img = cap.read()
+
+    h, w, c = overlayList[0].shape
+    img[0:h, 0:w] = overlayList[0]
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
